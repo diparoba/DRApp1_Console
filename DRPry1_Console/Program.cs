f651c1bed72afa25386344ad6f74ed7b0be6a9cf
Diego Robalino <kira_diparoba3@hotmail.com>
2022-05-01 10:13:37 -0500

    Commit inicial

---
+++ DRPry1_Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRPry1_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<vehiculo> list = new List<vehiculo>()
+            { new vehiculo() { 
+            
+            
+            
+            
+            }};
+        }
+        
+    }
+}
---
+++ DRPry1_Console/vehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRPry1_Console
+{
+    internal class vehiculo
+    {
+        private String marca { get; set; }
+        private String modelo { get; set; }
+        private String color { get; set; }
+
+        public vehiculo()
+        {
+
+        }
+        
+    }
+}
